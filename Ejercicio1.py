5507d6ca109ce040c79daecd6f7446e401631a87
Jhonatan <leherciJhonathandavid@gmail.com>
2025-02-14 10:59:07 -0500

    REalizado en sena

---
+++ Ejercicio1.py
@@ -0,0 +1,17 @@
+#Un profesor diseña un cuesntonariocon n prefuntas, estima que para calificar cada pregunta requiere m minutos.
+#calificar cada pregunta requiere m minutos. Si el cuestionario se aplica a x estudiantes, cuánto tiempo horas y
+#minutos necesita para calificar todos los exámenes. Se debe validar que los datos a ingresados
+#deben ser positivos y si ingresa valores negativos debe mostrar un mensaje al usuario y
+#culminar el algoritmo.
+
+#Se realiza el input para que el profesor ingrese los valores 
+n=input(float("Ingrese la Cantidad de preguntas: "))
+m=input(int("Ingrese la Cantidad de minutos: "))
+x=input(int("Ingrese la cantidad de Estuiantes: "))
+# Se realiza la variable while
+if n >=0 or m >=0 or x >=0:
+    
+else:
+    print("Error, el valor ingresado debe ser Positivos")
+
+#Se hacen variable para poder realizar las conversiones y loa inputs
